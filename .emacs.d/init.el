59233e6e3a3ef62aaf136dc1ff04185247eb1aec
yuizho <yuizho3@gmail.com>
2018-04-22 12:55:00 +0900

    modify init.el to make section for Flycheck

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -519,6 +519,21 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 (add-hook 'haskell-mode-hook 'font-lock-mode)
 (add-hook 'haskell-mode-hook 'imenu-add-menubar-index)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Scala用の設定
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; scala-mode
+(require 'scala-mode)
+(add-to-list 'auto-mode-alist '("\.scala$" . scala-mode))
+;;
+;; ensime
+(require 'ensime)
+(add-hook 'scala-mode-hook 'ensime)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; FlyCheck用の設定
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -534,14 +549,3 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Scala用の設定
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; scala-mode
-(require 'scala-mode)
-(add-to-list 'auto-mode-alist '("\.scala$" . scala-mode))
-;;
-;; ensime
-(require 'ensime)
-(add-hook 'scala-mode-hook 'ensime)
