637b3a70490242a1efcc1c427e35efce2142f287
yuizho <yuizho3@gmail.com>
2020-10-04 23:17:46 +0900

    add zsh configs

--- forceLink.sh
+++ forceLink.sh
@@ -2,5 +2,7 @@
 
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
 
--- moveAndLink.sh
+++ moveAndLink.sh
@@ -2,9 +2,13 @@
 
 mv ~/.bash_profile ~/dotfiles/.bash_profile
 mv ~/.bashrc ~/dotfiles/.bashrc
+mv ~/.zprofile ~/dotfiles/.zprofile
+mv ~/.zshrc ~/dotfiles/.zshrc
 mv ~/.emacs.d ~/dotfiles/.emacs.d
 
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.emacs.d ~/.emacs.d
 
