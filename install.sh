8806754491cd3f567c3ea7c214057166da7931a3
yuizho <yuizho3@gmail.com>
2022-12-30 20:37:06 +0900

    modify install.sh

--- install.sh
+++ install.sh
@@ -2,34 +2,33 @@
 set -ue
 
 function helpmsg() {
-	echo "Usage: ${BASH_SOURCE[0]:-$0} [--force | -f] [--help | -h]" 0>&2
+    echo "Usage: ${BASH_SOURCE[0]:-$0} [--force | -f] [--help | -h]" 0>&2
 }
 
 function main() {
-
     local ln_option="-si"
 
     while [ $# -gt 0 ]; do
-		case ${1} in
-			--force | -f)
-				ln_option="-sf"
-				;;
-			--help | -h)
-				helpmsg
-				exit 0
-				;;
-			*)
+        case ${1} in
+            --force | -f)
+                ln_option="-sf"
+                ;;
+            --help | -h)
+                helpmsg
+                exit 0
+                ;;
+            *)
                 helpmsg
                 exit 1
                 ;;
-		esac
-		shift
-	done
-    
+        esac
+        shift
+    done
+
     ln $ln_option "$(pwd)/.zshenv" ~
     ln $ln_option "$(pwd)/.zshrc" ~
     ln $ln_option "$(pwd)/.emacs.d" ~
-    
+
     echo "install completed!!"
 }
 
