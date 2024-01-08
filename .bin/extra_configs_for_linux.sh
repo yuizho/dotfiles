4bef62ec41384b4ae06baf12bcecd098be446c0c
yuizho <yuizho3@gmail.com>
2024-01-08 21:30:21 +0900

    add configurations for x

--- .bin/extra_configs_for_linux.sh
+++ .bin/extra_configs_for_linux.sh
@@ -16,5 +16,7 @@ echo "=====> start extra configurations for linux"
 
 echo "=====> configurations for x"
 ln $ln_option "${dotfiles_abs_dir}/linux/x/.xprofile" ~/
+sudo cp "${dotfiles_dir}/linux/x/00-keyboard.conf" /etc/X11/xorg.conf.d/
+sudo cp "${dotfiles_dir}/linux/x/30-touchpad.conf" /etc/X11/xorg.conf.d/
 
 echo "=====> extra configurations for linux is complete!!"
