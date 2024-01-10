a022b12372bd3ba26ec16014cce7b8221b957ddb
yuizho <yuizho3@gmail.com>
2024-01-11 01:20:40 +0900

    modify leftwm config to add audio key binds

---
+++ .config/leftwm/configure_display.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -uex
+
+MAIN=$(xrandr | awk '$3=="primary" {print $1}')
+CONNECTED_DISPLAYS=$(xrandr | awk '$2=="connected" && $3!="primary" {print $1}')
+DICSONNECTED_DISPLAYS=$(xrandr | awk '$2=="disconnected" && $3!="primary" {print $1}')
+
+for CONNECTED in $CONNECTED_DISPLAYS; do
+    [[ $CONNECTED == DP* ]] && xrandr --output $MAIN --primary --output $CONNECTED --auto --right-of $MAIN
+    [[ $CONNECTED == HDMI* ]] && xrandr --output $MAIN --primary --output $CONNECTED --set audio on --auto --right-of $MAIN
+done
+
+for DISCONNECTED in $DICSONNECTED_DISPLAYS; do
+    xrandr --output $DISCONNECTED --off
+done
+
