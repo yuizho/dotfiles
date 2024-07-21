cda67e39421a530cd56280547f14ed1683928b27
yuizho <yuizho3@gmail.com>
2024-07-21 21:51:06 +0900

    modify display config for linux

--- .config/leftwm/configure_display.sh
+++ .config/leftwm/configure_display.sh
@@ -7,7 +7,7 @@ CONNECTED_DISPLAYS=$(xrandr | awk '$2=="connected" && $3!="primary" {print $1}')
 DICSONNECTED_DISPLAYS=$(xrandr | awk '$2=="disconnected" && $3!="primary" {print $1}')
 
 for CONNECTED in $CONNECTED_DISPLAYS; do
-    [[ $CONNECTED == DP* ]] && xrandr --output $MAIN --primary --output $CONNECTED --auto --right-of $MAIN
+    [[ $CONNECTED == DP* ]] && xrandr --output $MAIN --primary --output $CONNECTED --auto --right-of $MAIN --mode 3840x2160
     [[ $CONNECTED == HDMI* ]] && xrandr --output $MAIN --primary --output $CONNECTED --set audio on --auto --right-of $MAIN
 done
 
