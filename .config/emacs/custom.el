30cc41525544f2fa0cbef4c34b59d68049be2ea4
yuizho <yuizho@yuizhonoMacBook-Air.local>
2025-04-11 12:41:05 +0900

    Revert "modify emacs configs"
    This reverts commit 6229c00f546e15bc7bc8bc5e1ef8fa6969cdaa5a.

--- .config/emacs/custom.el
+++ .config/emacs/custom.el
@@ -7,7 +7,8 @@
    '(("org" . "https://orgmode.org/elpa/")
      ("melpa" . "https://melpa.org/packages/")
      ("gnu" . "https://elpa.gnu.org/packages/")))
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(el-get hydra leaf-keywords leaf yasnippet wgrep web-mode tuareg rust-mode projectile pos-tip package-utils magit lua-mode lsp-mode js2-mode htmlize helm haskell-mode git-gutter+ flycheck erlang epc company)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
